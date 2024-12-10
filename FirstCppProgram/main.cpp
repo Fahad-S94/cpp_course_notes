d0a3cc78d8728b1d7fde828e8f4bb6aa5a64251d
Fahad Shoaib <f.shoaib1580@gmail.com>
2024-12-10 02:27:29 -0500

    Input Output

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 //function sample
 int addNumbers(int first_number, int second_number){
@@ -16,6 +17,10 @@ int main(){
     std::cout << "Fahad Shoaib" << std::endl;
     std::cout << "Fahad Shoaib";
     std::cout << "Fahad Shoaib";
+
+    
+    /*()*/
+
 
     // Without Function
     int first_number {3}; // Statement
@@ -37,6 +42,41 @@ int main(){
     //Without storing variable
     std:: cout << "Added Sum: " << addNumbers(5, 4) << std::endl;
 
+    /*()*/
+
+    // std::cout = Printing stuff to console
+    //std::cin = user input
+    //std::cerr = Error
+    //std::clog = Log message
+    // << = stream out
+
+    //Example
+    //Printing Data
+    std:: cout << "Hello C++23" << addNumbers(5, 4) << std::endl; // Our data is going from our program (int main()) to std::cout which will dump it out to the terminal
+
+    std::cerr << "Error message : Something is wrong" << std::endl;
+    std::clog << "Log message : Something happpened here" << std::endl;
+
+    int age1;
+    std::string name;
+
+    std:: cout << "Please type name and age: " << std::endl;
+    std::cin >> name; // >> stream = means takes the date from terminal and assigns it to Variable
+    std::cin >> age1;
+    //std::cin >> name >> age1;
+
+    std:: cout << "Hello! " << name << " You are " << age1 << " years old." << std::endl;
+
+    //Data with Spaces
+    std::string full_name;
+    int age3;
+
+    std:: cout << "Please type full name and age: " << std::endl;
+
+    std::getline(std::cin,full_name);
+    std::cin >> age3;
+
+    std:: cout << "Hello! " << full_name << " You are " << age3 << " years old." << std::endl;
 
     return 0;
 }
