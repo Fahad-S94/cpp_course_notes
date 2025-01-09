2753ffcaf76cd633f7213ae0f5c74dd359386880
Fahad Shoaib <f.shoaib1580@gmail.com>
2025-01-09 02:53:33 -0500

    Character Manipulation and Strings 2

--- 10. Character Manipulation And Strings/main.cpp
+++ 10. Character Manipulation And Strings/main.cpp
@@ -124,8 +124,307 @@ int main(){
     
     std::cout << "Lowercase string : " << dest_str << std::endl;
 
-    //! C-String Manipulation
+    //! C-String Manipulation 
+    // https://en.cppreference.com/w/cpp/header/cstring
     
+    /*
+	//* std::strlen : Find the length of a string, real arrays and those decayed into pointers
+    const char message1 [] {"The sky is blue."};
+	
+	//* Array decays into pointer when we use const char*
+	const char* message2 {"The sky is blue."};
+    std::cout << "message1 : " << message1 << std::endl;
+	
+	//* strlen ignores null character(spaces)
+    std::cout << "strlen(message1) : " << std::strlen(message1) << std::endl;
+	
+	//* Includes the null character
+    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl;
+	
+	//* strlen still works with decayed arrays
+	std::cout << "strlen(message2) : " << std::strlen(message2) << std::endl;
+	
+	//* Prints size of pointer // 8 because all pointers are 8bytes
+    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl;
+    */
+
+
+   //* std::strcmp - signature : int strcmp( const char *lhs, const char *rhs ); // Returns negative value if lhs appears before rhs in lexicographical order, //Zero if lhs and rhs compare equal. //and Positive value if lhs appears after rhs in lexicographical order. 
+    /*
+    std::cout << std::endl;
+    std::cout << "std::strcmp : " << std::endl;
+    const char* string_data1{ "Alabama" };
+    const char* string_data2{ "Blabama" };
+
+    char string_data3[]{ "Alabama" };
+    char string_data4[]{ "Blabama" };
+
+    //Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
+        << std::strcmp(string_data1, string_data2) << std::endl;
+
+    std::cout << "std::strcmp (" << string_data3 << "," << string_data4 << ") : "
+        << std::strcmp(string_data3, string_data4) << std::endl;
+
+
+    string_data1 = "Alabama";
+    string_data2 = "Alabamb";
+
+    //string_data3 = "Alabama";
+    //string_data4 = "Alabamb";
+
+    //Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : " << std::strcmp(string_data1, string_data2) << std::endl;
+
+
+    string_data1 = "Alacama";
+    string_data2 = "Alabama";
+
+    //Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : " << std::strcmp(string_data1, string_data2) << std::endl;
+
+    string_data1 = "India";
+    string_data2 = "France";
+
+    //Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
+        << std::strcmp(string_data1, string_data2) << std::endl;
+
+    string_data1 = "Kigali";
+    string_data2 = "Kigali";
+
+    //Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
+        << std::strcmp(string_data1, string_data2) << std::endl;
+    */
+
+
+	//* std::strncmp : int strncmp( const char *lhs, const char *rhs, std::size_t count ); //Compares n characters in the strings //Returns : Negative value if lhs appears before rhs in lexicographical order. //Zero if lhs and rhs compare equal, or if count is zero.  //Positive value if lhs appears after rhs in lexicographical order. 
+    /*
+    //Print out the comparison
+
+    const char* string_data1{ "Alabama" };
+    const char* string_data2{ "Blabama" };
+    size_t n{3};
+    std::cout << std::endl;
+    std::cout << "std::strncmp : " << std::endl;
+    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : " 
+              << std::strncmp(string_data1,string_data2,n) << std::endl;
+              
+    string_data1 = "aaaia";
+    string_data2 = "aaance";
+    
+    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : " 
+              << std::strncmp(string_data1,string_data2,n) << std::endl;
+              
+              
+    n = 5;
+    
+    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : " 
+              << std::strncmp(string_data1,string_data2,n) << std::endl;
+              
+    string_data1 = "aaaoa";
+    string_data2 = "aaance";
+    
+    
+    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : " 
+              << std::strncmp(string_data1,string_data2,n) << std::endl;
+    */
+
+
+
+    //Find the first occurrence of a character 
+	//CODE STOLEN FROM THE DOCS : 
+    /*
+    std::cout << std::endl;
+    std::cout << "std::strchr : " << std::endl;
+	//doc : https://en.cppreference.com/w/cpp/string/byte/strchr
+    
+	//* we use std::strchr to find all the characters one by one.
+
+	const char * const str { "Try not. Do, or do not. There is no try."};
+    //? Can make this a const pointer to prevent users from making it point somewhere else
+	char target = 'T';
+	const char *result = str;
+	size_t iterations{};
+	
+ 
+	while ((result = std::strchr(result, target)) != nullptr) {
+		std::cout << "Found '" << target << "' starting at '" << result << "'\n";
+ 
+		// Increment result, otherwise we'll find target at the same location
+		++result;
+		++iterations;
+	}
+	std::cout << "iterations : " << iterations << std::endl;
+    */
+
+
+    //* A lesser version that will loop 25 times // not good code, won't work
+    /*
+	//Find the first occurrence of a character 
+	//CODE STOLEN FROM THE DOCS : 
+	std::cout << std::endl;
+	std::cout << "std::strchr : " << std::endl;
+	//doc : https://en.cppreference.com/w/cpp/string/byte/strchr
+
+	//we use std::strchr to find all the characters one by one.
+
+	const char* str{ "Try not. Do, or do not. There is no try." };
+	char target = 'T';
+	const char* result{ nullptr };
+	size_t iterations{};
+
+
+	while ((result = std::strchr(str, target)) != nullptr) {
+		std::cout << "Found '" << target
+			<< "' starting at '" << result << "'\n";
+
+		// Increment result, otherwise we'll find target at the same location
+		++str; // Making the pointer point to the next character
+		++iterations;
+	}
+	std::cout << "iterations : " << iterations << std::endl;
+    */
+
+
+	//* Find last occurence
+	//CODE STOLEN FROM THE DOCS : 
+    std::cout << std::endl;
+    std::cout << "std::strrchr : " << std::endl;
+	//doc : https://en.cppreference.com/w/cpp/string/byte/strrchr
+	
+    char input[] = "/home/user/hello.cpp";
+    char* output = std::strrchr(input, '/');
+    if(output)
+        std::cout << output+1 << std::endl; 
+        //+1 because we want to start printing past 
+        // the character found by std::strrchr.
+    
+
+    //! C-String Concatenation And Copying
+
+    //* Concatenation (std::strcat)
+    /*
+	std::cout << std::endl;
+	std::cout<< "std::strcat : " << std::endl;
+	//doc : https://en.cppreference.com/w/cpp/string/byte/strcat
+	
+	char dest[50] = "Hello ";
+    char src[50] = "World!";
+    std::strcat(dest, src); //* Some compilers (MSVC) think these functions are unsafe!
+    std::cout << "dest : " << dest << std::endl; // Hello World
+    std::strcat(dest, " Goodbye World!"); 
+    std::cout << "dest : " << dest << std::endl; // Hello World Goodbye World!
+    //* the appended variable can't be a const because it has to be modifiable. (std::strcat(x, y), x's value will change to x=x+y)
+    */
+
+
+	//More concatenation
+    /*
+	std::cout << std::endl;
+	std::cout << "More std::strcat : " << std::endl;
+	
+    char *dest1 = new char[30]{'F','i','r','e','l','o','r','d','\0'};
+    char *source1 = 
+        new char[30]{',','T','h','e',' ','P','h','e','n','i','x',' ','K','i','n','g','!','\0'};
+		
+	//NOTE TO SELF : 
+	//* Describe what's going to happen in std::strcat before you do the cat //source is going to be appended to dest, //? will start by overriding the null character in dest
+			
+    std::cout << "std::strlen(dest1) :" << std::strlen(dest1) << std::endl;
+    std::cout << "std::strlen(source1) : " << std::strlen(source1) << std::endl;
+    
+	std::cout << "Concatenating..." << std::endl;
+    std::strcat(dest1,source1);
+    
+    std::cout << "std::strlen(dest1) :" << std::strlen(dest1) << std::endl;
+    std::cout << "dest1 : " << dest1 << std::endl;
+    */
+
+
+    //* std::strncat  : concatenates n characters from src to dest and returns a pointer to the result string
+    //signature : char *strncat( char *dest, const char *src, std::size_t count );
+    /*
+    std::cout << std::endl;
+    std::cout << "std::strncat:" << std::endl;
+    char dest2[50] { "Hello" };
+    
+    char source2[30] = {" There is a bird on my window"};
+    
+    //You can even use the returned pointer immediately for print out
+    //This is a pattern you'll see a lot in C++ code out there.
+    std::cout << std::strncat(dest2,source2,6) << std::endl;
+    
+    //Or you can do std::strncat separately and print dest2
+    std::strncat(dest2,source2,6);
+    std::cout << "The concatenated string is : " << dest2 << std::endl;
+    */
+
+
+    //* std::strcpy - signature : char* strcpy( char* dest, const char* src );
+    /*
+    std::cout << std::endl;
+	std::cout << "std::strcpy : " << std::endl;
+    const char* source3 = "C++ is a multipurpose programming language.";
+    char *dest3 = new char[std::strlen(source3) + 1];
+    // +1 because std::strlen does NOT account for the null character
+	//Contains garbage data
+	//Not initialized
+
+    std::strcpy(dest3,source3);
+    
+    std::cout << "sizeof(dest3) : " << sizeof(dest3) << std::endl;
+    std::cout << "std::strlen(dest3) : " << std::strlen(dest3) << std::endl;
+    std::cout << "dest3 : " << dest3 << std::endl;
+    */
+
+
+    //* std::strncpy : Copy n characters from src to dest -
+    //signature : char *strncpy( char *dest, const char *src, std::size_t count );
+	std::cout << std::endl;
+    std::cout << "std::strncpy:" << std::endl;
+    const char* source4 = "Hello";
+    char dest4[] = {'a', 'b', 'c', 'd', 'e', 'f','\0'}; //* Have to put the terminating null char if we want to print
+    
+    std::cout << "dest4 : " << dest4 << std::endl;
+    
+	std::cout << "Copying..." << std::endl;
+    std::strncpy(dest4,source4,5);
+    
+    std::cout << "dest4 : " << dest4 << std::endl;
+
+    //! Declaring And Using std::string
+
+    std::string full_name;//Empty string
+    std::string planet {"Earth. Where the sky is blue"};//Initialize with string literal
+    std::string prefered_planet{planet};//Initialize with other existing string
+    std::string message {"Hello there",5};	//Initialize with part of a string literal. //Contains hello
+    std::string weird_message(4,'e');//Initialize with multiple copies of a char
+                                        // contains eeee
+    std::string greeting{"Hello World"};
+    std::string saying_hello{ greeting,6,5};//Initialize with part of an existing std::string
+                                            // starting at index 6, taking 5 characters.
+                                            //Will contain World.
+
+    std::cout << "full_name : " << full_name << std::endl;
+    std::cout << "planet : " << planet << std::endl;
+    std::cout << "prefered_planet : " << prefered_planet << std::endl;
+    std::cout << "message : " << message << std::endl;
+    std::cout << "weird_message : " << weird_message << std::endl;
+    std::cout << "greeting : " << greeting << std::endl;
+    std::cout << "saying_hello : " << saying_hello << std::endl;
+
+    //* Changing std::string at runtime
+    planet = "Earth. Where the sky is blue Earth. Where the sky is blue Earth. Where ";
+    std::cout << "planet : " << planet << std::endl;
+
+    //Use a raw array 
+    const char * planet1 {"Earth. Where the sky is blue Earth."};
+    planet1 = "Earth. Where the sky is blue Earth. Where the sky is blue Earth. Where ";
+    std::cout << "planet1 : " << planet1 << std::endl;
+    //? in this case, the planet1 pointer does change the array to the new string but the original string still stays in memory (WASTED out). std::string changes the array but deletes the old array so there's no memory loss
+
 
     return 0;
 }
